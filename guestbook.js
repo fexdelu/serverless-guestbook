ce4007ced13df3628cb46a74639841f95937970d
Federico Lumma <federico.lumma@gmail.com>
2022-07-01 15:23:21 -0300

    update API url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://a9c33bc4.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
